54a489c9f0940bb5b4bd0bff5aaf6baa0b3cb06f
Michael Wittig <michael@widdix.de>
2019-07-11 16:16:41 +0200

    init (#1)

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const test = require('ava');
+const cfntest = require('@cfn-modules/test');
+
+test.serial('defaults', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
+test.serial('with-secret', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/with-secret.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
